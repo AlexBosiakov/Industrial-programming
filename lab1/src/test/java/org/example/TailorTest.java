dfbfed721c3a08b4d7c095b8d2435c435bcd1de0
AlexBosiakov <137886862+AlexBosiakov@users.noreply.github.com>
2024-01-15 17:11:08 +0300

    Add files via upload

---
+++ lab1/src/test/java/org/example/TailorTest.java
@@ -0,0 +1,20 @@
+package org.example;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TailorTest {
+    void calculateMySolution1() {
+        Tailor A= new Tailor(0.99,3);
+
+    }
+    void calculateMySolution2() {
+
+    }
+    void calculateMySolution3() {
+
+    }
+    void calculateBigDecimal() {
+    }
+}
